f2c0af82f196e1de507a2c2fc57e4bf81de82e10
weaver312 <43344069+weaver312@users.noreply.github.com>
2021-08-25 19:56:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,19 +2,7 @@
 # 生成hexo静态文件
 echo 'step1'
 npm install hexo-cli
-echo 'step2'
-ls -l /usr/local/bin/hexo
 echo 'step3'
-ls ./
+ls ./hexo-cli/bin/hexo
 echo 'step4'
-ls ../
-echo 'step5'
-ls ../..
-echo 'step6'
-ls /usr/
-echo 'step7'
-ls /usr/bin/
-pwd
-ls /usr/bin/
-echo 'step9'
-/usr/local/bin/hexo generate
+asdfasdfasdf
