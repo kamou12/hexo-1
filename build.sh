d4e0d3df5c55d00b238021c1f011dca802438398
kamou12 <86824325+kamou12@users.noreply.github.com>
2021-09-27 11:27:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-# 生成hexo静态文件
-npm install hexo-cli
-echo 'step1'
-ls ./node_modules/hexo-cli/bin/
-echo 'step1'
-npm install
-echo 'step1'
-./node_modules/hexo-cli/bin/hexo generate
+hexo generate
